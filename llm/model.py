b4ef86bd95ebce08dec5ca5c4be7fcadd2976d8e
RavenRT02 <praveenkumart261@gmail.com>
2026-07-06 22:16:31 +0530

    Fixed ML dependencies issue and pinned versions

--- llm/model.py
+++ llm/model.py
@@ -65,7 +65,7 @@ def load_model(model_name):
     # The Transformers library automatically detects available hardware and decides where each layer should go. (GPU or CPU or both)
     # If device_map is not set, it loads the layers onto CPU unless model.to("cuda") is specified somewhere
     model = AutoModelForCausalLM.from_pretrained(model_name, device_map="auto", quantization_config=quantization_config)
-    print(model.hf_device_map)
+    # print(model.hf_device_map) - uncomment to check where the model is placed for testing "{'': 0}" is Cuda 0
 
     # .eval() one of Pytorch modes used for inference (other is training .train())
     # Some neural network layers behave differently during training and inference. Std practice to use model.eval() for inference.
